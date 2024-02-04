230c0c27b31677f54d99f68e407b5374199fdf7a
DoomPI <rslomtev@edu.hse.ru>
2024-02-05 00:02:29 +0500

    минорные правки

--- base/divkit/data/src/iosMain/kotlin/ru/pyroman/laza/base/divkit/data/parser/ScreenDataParser.kt
+++
@@ -1,9 +0,0 @@
-package ru.pyroman.laza.base.divkit.data.parser
-
-import ru.pyroman.laza.base.divkit.domain.model.ScreenData
-
-actual class ScreenDataParser {
-    actual fun parse(jsonString: String): ScreenData {
-        TODO("Not yet implemented")
-    }
-}
